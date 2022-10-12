709b793184ed63236df202ee7966ebdc3f944728
wpaq <wallysonpabloo@gmail.com>
2022-10-12 19:38:27 -0300

    feat: create .dockerignore and configured database connection with docker

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,9 @@
 import mongoose from 'mongoose';
 
-// Database Connection
-mongoose
-  .connect(
-    'mongodb://localhost:27017/spaceflight-news-challenge',
-  )
+const mongoURI = 'mongodb://root:admin88@localhost/spaceflight-news-challenge?authSource=admin';
+
+mongoose.connect(mongoURI, { useNewUrlParser: true })
   .then(() => {
     console.log('Connected to Database!')
-  })
-  .catch((err) => console.log(err))
+    })
+  .catch((err) => console.log(err));
